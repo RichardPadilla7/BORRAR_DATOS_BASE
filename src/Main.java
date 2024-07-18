500fdf1ec88b94272102e5b8e551bdee6fa1b0c2
Richard Padilla <Richard>
2024-07-18 08:25:21 -0500

    12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // JDBC URL, usuario y contraseña de MySQL
+        String URL = "jdbc:mysql://localhost:3306/estudiantes2024A";
+        String USER = "root";
+        String PASSWORD = "123456";
+
+        String cedula = "1726195207";
+
+        String sql = "DELETE FROM estudiante WHERE cedula = ?";
+
+        try (
+                Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+                PreparedStatement statement = conn.prepareStatement(sql)
+        ) {
+            statement.setString(1, cedula);
+
+            int filasEliminadas = statement.executeUpdate();
+
+            if (filasEliminadas > 0) {
+                System.out.println("El estudiante " + cedula + " ha sido eliminado correctamente.");
+            } else {
+                System.out.println("No se encontró ningún estudiante con cedula " + cedula + " para eliminar.");
+            }
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
